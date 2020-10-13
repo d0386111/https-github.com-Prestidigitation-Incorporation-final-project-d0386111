10570b0a706f1a584f075c53aaa3b03c18893d80
Dustan Francis <d0386111@amarillocollege.com>
2020-10-13 15:33:33 -0500

    More progress

--- Final/ViewController.swift
+++ Final/ViewController.swift
@@ -14,7 +14,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+//Linked my button
+    @IBAction func Generate(_ sender: Any) {
+        randomize()
+    }
+//Establish array/randomizer
+    var subjects = ["He", "Him", "Her", "The", "They", "Them", "That", "My", "Your", "Our", "John", "Don", "Ron", "Becky", "Karen", "Susan", "Bob", "Jane", "Joe"]
+    var nouns = ["Table", "Rock", "Lake", "Counter", "Shoe", "Faucet", "Remote", "Phone", "Pen", "Drink", "Chair", "Book", "Door", "Window", "Freedom", "Basketball", "Disneyland", "Trump", "Bush", "Obama", "Food", "Console", "Bed"]
+    var objects = ["the guitar", "his part", "the President", "the hill", "the kite", "a mouse", "in town", "the book", "the solution", "the problem", "cooking roast", "our dogs", "the door", "the stairs", "the window", "freedom"]
+    func randomize() {
+        let result = (subjects.randomElement() != nil) && (nouns.randomElement() != nil) && (objects.randomElement() != nil)
+        print("Your random PassPhrase is: '\(result)'.")
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
